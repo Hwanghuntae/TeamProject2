e8cb5a3791634425810071319dc67603a9a55f3a
Hwang <ghkdgnsxo2484@naver.com>
2019-09-09 13:57:48 +0900

    Circle클래스 생성자 생성

--- base/Circle.java
+++ base/Circle.java
@@ -9,27 +9,10 @@ package base;
  */
 public class Circle
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * Circle 클래스의 객체 생성자
-     */
-    public Circle()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    private int x, y, radius;
+    public Circle(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;		
+        this.radius = radius;		
     }
 }
