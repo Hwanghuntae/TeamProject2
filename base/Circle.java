2129aa15e4120cf8618c7029642f6a934442cb51
Hwanghuntae <ghkdgnsxo2484@naver.com>
2019-09-10 20:33:56 +0900

    a

--- base/Circle.java
+++ base/Circle.java
@@ -1,22 +1,13 @@
 package base;
-import MyInterface.Shape;
+
+
 /**
  * 여기에 Circle 클래스 설명을 작성하십시오.
  * 
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class Circle implements Shape
+public class Circle
 {
-    private int x, y, radius;
-    public Circle(int x, int y, int radius) {
-        this.x = x;
-        this.y = y;		
-        this.radius = radius;		
-    }
     
-    public boolean equals(Object obj)
-    {
-        
-    }
 }
