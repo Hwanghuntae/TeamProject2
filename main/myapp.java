8f466ff8611d2cc255306c0388790d573fbd1838
yunjae <coygyj@gmail.com>
2019-09-10 22:31:56 +0900

    .

--- main/myapp.java
+++ main/myapp.java
@@ -14,10 +14,10 @@ public class myapp
         Circle c2 = new Circle (2,4,10);
         System.out.println("원1 :"+ c1);
         System.out.println("원2 :"+ c2);
+        
         if(c1.equals(c2))
             System.out.println("같은 원");        
         else    
             System.out.println("서로 다른 원");
-
     }
 }
